1ce2ac71fda6bdc9c36c4fc4cc9eb673c99acdb6
OsParking <osparking@users.noreply.github.com>
2020-03-13 22:48:24 +0900

    Joho changes

--- github-from-eclipse-arti/src/main/java/com/javatechie/github_from_eclipse_arti/App.java
+++ github-from-eclipse-arti/src/main/java/com/javatechie/github_from_eclipse_arti/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "change from Java John");
         System.out.println( "Hello World!" + "First change from Java Techie");
     }
 }
